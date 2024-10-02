6d3f6489dc0b9395ccdd40b3bac4a915e897de78
castilloeme <emecasti65@gmail.com>
2024-10-01 19:36:23 -0500

    Modificando ancho del contenido de cafes

--- components/main-cafes.jsx
+++ components/main-cafes.jsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 export default function MainCafes() {
   return (
     <section className="mb-16">
-      <div className="lg:max-w-[70rem] md:max-w-[45rem] max-w-[27rem] mx-auto bg-[#1B1D1F] -mt-[10rem] px-5 lg:px-14 lg:py-16 md:px-10 md:py-12 py-10 rounded-2xl relative overflow-hidden">
+      <div className="lg:max-w-[75rem] md:max-w-[45rem] max-w-[27rem] mx-auto bg-[#1B1D1F] -mt-[10rem] px-5 lg:px-14 lg:py-16 md:px-10 md:py-12 py-10 rounded-2xl relative overflow-hidden">
         <Image
           src="/vector.svg"
           alt="vector"
