27fe03519c5a4c4dcdb52be2e15ec9b24d412eea
castilloeme <emecasti65@gmail.com>
2024-10-01 19:42:11 -0500

    Modificando alto de la imagen hero

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import MainCafes from "@/components/main-cafes";
 export default function Home() {
   return (
     <>
-      <div className="bg-[url(/bg-cafe.jpg)] bg-no-repeat bg-cover bg-center h-[37vh] w-full" />
+      <div className="bg-[url(/bg-cafe.jpg)] bg-no-repeat bg-cover bg-center h-[45vh] w-full" />
       <MainCafes />
     </>
   );
